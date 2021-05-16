f5ce4aa807879c704c7c4c0408045653b504943d
karenxzhou <zhoukaren2001@gmail.com>
2021-05-16 01:36:25 -0400

    added ghost

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     $(".rug").mouseover(moveAcross);
     $(".rug").mouseleave(goBack);
 
+    $("#pink").click(makePink);
+
+    $("#darkpink").click(makeDarkpink);
+    
+    $("#puppy").click(puppyShow);
+    
+    function puppyShow() {
+        $(".image-container").toggleClass("showing");
+    }
+
     function moveAcross() {
         // $(".bunnies").show();
         $(".bunnies").addClass("moving");
@@ -12,6 +22,23 @@ $(document).ready(function() {
         $(".bunnies").removeClass("moving");
     }
     
+
+    function makePink() {
+        $(".ghostContainer2").toggleClass("pink");
+    }
+
+
+
+
+    function makeDarkpink() {
+        $(".ghost").toggleClass("darkpink");
+        $(".ghost").removeClass("ghost");
+    }
+
+    function makePink() {
+        $(".ghost").toggleClass("pink");
+        $(".ghost").removeClass("ghost")
+    }
 })
 
 console.log("test");
