a4c7ad1400605afd667f0653f1b9cd1d6528f5d3
karenxzhou <zhoukaren2001@gmail.com>
2021-05-20 22:23:40 -0400

    made buttons work

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
 $(document).ready(function() {
     $(".rug").mouseover(moveAcross);
     $(".rug").mouseleave(goBack);
-
+    
+    // $("#pinkpup").click(puppyShow);
     $("#pink").click(makePink);
-
     $("#darkpink").click(makeDarkpink);
+    $("#red").click(makeRed);
+    $("#orange").click(makeOrange);
+    $("#yellow").click(makeYellow);
+    $("#green").click(makeGreen);
+    $("#blue").click(makeBlue);
+    $("#purple").click(makePurple);
     
-    $("#puppy").click(puppyShow);
+    $(".ghostButton").hover(changeBackground);
     
-    function puppyShow() {
-        $(".image-container").toggleClass("showing");
+    
+    // function puppyShow() {
+    //     $(".image-container").toggleClass("showing");
+    // }
+
+    function makePink() {
+        $(".image-container2").toggleClass("showing");
+    }
+
+    function makeDarkpink() {
+        $(".image-container3").toggleClass("showing");
+    }
+
+    function makeRed() {
+        $(".image-container4").toggleClass("showing");
+    }
+
+    function makeOrange() {
+        $(".image-container5").toggleClass("showing");
+    }
+
+    function makeYellow() {
+        $(".image-container6").toggleClass("showing");
+    }
+
+    function makeGreen() {
+        $(".image-container7").toggleClass("showing");
+    }
+
+    function makeBlue() {
+        $(".image-container8").toggleClass("showing");
+    }
+
+    function makePurple() {
+        $(".image-container9").toggleClass("showing");
+    }
+
+    function changeBackground() {
+        $(".ghostButton").removeClass("changed");
+        $(this).toggleClass("changed");
     }
 
     function moveAcross() {
@@ -23,22 +67,22 @@ $(document).ready(function() {
     }
     
 
-    function makePink() {
-        $(".ghostContainer2").toggleClass("pink");
-    }
+    // function makePink() {
+    //     $(".ghostContainer2").toggleClass("pink");
+    // }
 
 
 
 
-    function makeDarkpink() {
-        $(".ghost").toggleClass("darkpink");
-        $(".ghost").removeClass("ghost");
-    }
+    // function makeDarkpink() {
+    //     $(".ghost").toggleClass("darkpink");
+    //     $(".ghost").removeClass("ghost");
+    // }
 
-    function makePink() {
-        $(".ghost").toggleClass("pink");
-        $(".ghost").removeClass("ghost")
-    }
+    // function makePink() {
+    //     $(".ghost").toggleClass("pink");
+    //     $(".ghost").removeClass("ghost")
+    // }
 })
 
 console.log("test");
